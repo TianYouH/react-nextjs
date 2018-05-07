40437a3a96ac183e0c76d6395c1f73c4805d0ba1
huangjinliang <1406957473@qq.com>
2018-05-07 16:35:47 +0800

    入口文件更新

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import Root from './Router'
+import App from './App'; //单例测试
+import Root from './Router'; //引入router测试（）
 import registerServiceWorker from './registerServiceWorker';
 import 'antd-mobile/dist/antd-mobile.css'
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
