8a6946ecc6155616da134f1f28aca4ee4a45efea
huangjinliang <huangjinliang@zto.cn>
2019-08-06 21:39:42 +0800

    临时提交

--- src/App.js
+++ src/App.js
@@ -5,68 +5,57 @@ import {
   Link
 } from 'react-router-dom'
 
-const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-      </ul>
+const App = React.createClass({
+  render() {
+    return (
+      <Router>
+        <Route path="/" component={Home}>
+          <Route path="about" component={About} />
+          <Route path="inbox" component={Inbox}>
+            <Route path="messages/:id" component={Message} />
+          </Route>
+        </Route>
+      </Router>
+    )
+  }
+})
 
-      <hr/>
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topics}/>
-    </div>
-  </Router>
-)
+const About = React.createClass({
+  render() {
+    return <h3>About</h3>
+  }
+})
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+const Inbox = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Inbox</h2>
+        {this.props.children || "Welcome to your Inbox"}
+      </div>
+    )
+  }
+})
 
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:topicId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-)
+const Message = React.createClass({
+  render() {
+    return <h3>Message {this.props.params.id}</h3>
+  }
+})
 
 export default App;
