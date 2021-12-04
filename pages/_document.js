0eaf6bfef513c33805a7e4758ae8d64b9ff694ad
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-12-04 17:48:45 +0800

    feat: 提交style-jsx测试

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+// 如果存在该文件，该文件会被默认引用
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -10,12 +11,13 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>我的 App</title>
+          {/* 不推荐在document中定义title,可以通过在页面中 import Head from 'next/head' 方式定义 */}
+          {/* <title>我的 App</title>
           <style>
             {`.test {
               color: red;
             }`}
-          </style>
+          </style> */}
         </Head>
         <body className="test" >
           <Main />
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
 import Link from "next/link";
-import { Router } from 'next/router';
+import { Router } from "next/router";
 import { Button } from "antd";
 
- function App () {
+const color = "blue"
+function App() {
   return (
-    <div>
-      你好世界
-    </div>
+    <>
+      <div>
+        App 页面信息
+        <p>App p 11111</p>
+        <span className="sp glo" >App span 22222</span>
+      </div>
+      {/* 正常局部测试 */}
+      <style jsx>{`
+        div {
+          color: red;
+        }
+        p {
+          color: ${color};
+        }
+        .sp {
+          color: yellow
+        }
+      `}</style>
+      {/* 全局特性测试,当该组件移除全局效果也会失效 */}
+      <style jsx global>{`
+        .glo {
+          color: green
+        }
+      `}</style>
+    </>
   );
-};
+}
 
 export default App;
