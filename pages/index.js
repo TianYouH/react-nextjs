a75329d8ee7af68ea9914cec3dd3cf7bc1403738
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2021-12-24 16:51:34 +0800

    “feat: 提交 懒加载组件测试

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Link from "next/link";
 import { Router } from "next/router";
 import { Button } from "antd";
+// import moment from 'moment';
 
 const color = "blue"
-function App() {
+function App({name, time}) {
   return (
     <>
       <div>
         App 页面信息
         <p>App p 11111</p>
         <span className="sp glo" >App span 22222</span>
+        <p>我叫{name},在{time}是大佬</p>
       </div>
       {/* 正常局部测试 */}
       <style jsx>{`
@@ -32,5 +34,19 @@ function App() {
     </>
   );
 }
+
+
+App.getInitialProps = async (ctx) => {
+  const moment = await import("moment");
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      resolve({
+        name: "黄长生",
+        time: moment.default(Date.now() - (1000*60*60)).fromNow()
+      })
+    }, 1000);
+  })
+  return promise;
+};
 
 export default App;
