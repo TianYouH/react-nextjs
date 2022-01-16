d9b6c93e33e55daf33ef9926bc3d4341875fbc43
DESKTOP-ER5718D\zt <huangjinliang@zto.com>
2022-01-16 13:21:30 +0800

    提交store代码

--- store/store.js
+++ store/store.js
@@ -1,12 +1,17 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 const initialState = {
   count: 0,
 };
 
+const userInitialState = {
+  username: "huang",
+};
+
 const ADD = "ADD";
 
-function reducer(state = initialState, action) {
+function counterReducer(state = initialState, action) {
   switch (action.type) {
     case ADD:
       // 一定要返回新的对象，否则可能会导致页面更新失败
@@ -18,6 +23,29 @@ function reducer(state = initialState, action) {
   }
 }
 
-const store = createStore(reducer, initialState);
+const UPDATE_USERNAME = "UPDATE_USERNAME";
+
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case UPDATE_USERNAME:
+      return {
+        ...state,
+        username: action.name,
+      };
+    default:
+      return state;
+  }
+}
+
+const allReducers = combineReducers({
+  counter: counterReducer,
+  user: userReducer,
+});
+
+// const store = createStore(counterReducer, initialState);
+const store = createStore(allReducers, {
+  counter: initialState,
+  user: userInitialState,
+});
 
 export default store;
